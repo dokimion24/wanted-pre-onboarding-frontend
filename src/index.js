a01cb164ea1602edd45a7d257e8d81fab63aa792
dokimion24 <leecy3610@gmail.com>
2023-06-02 21:14:09 +0900

    feat: index.js GlobalStyle 컴포넌트 추가

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { GlobalStyle } from "./styles/global";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <GlobalStyle />
+    <App />
+  </BrowserRouter>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
