c595aad14ee801a80e49b394a75ca6e3c37ae616
dokimion24 <leecy3610@gmail.com>
2023-06-03 00:45:34 +0900

    design: 회원가입 폼 스타일 지정

---
+++ src/components/SignupForm/style.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+  padding: 0 32px;
+`;
+
+export const IconWrapper = styled.div`
+  font-size: 32px;
+  display: flex;
+  margin-bottom: 32px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SigninText = styled.div`
+  margin-left: 8px;
+`;
+
+export const InputArea = styled.div`
+  margin-bottom: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Label = styled.label`
+  width: 100%;
+  font-size: 12px;
+  margin: 4px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 40px;
+
+  &:focus {
+  }
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: end;
+`;
