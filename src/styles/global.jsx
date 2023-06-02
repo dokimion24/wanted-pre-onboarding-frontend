af77e1d7d90ad5a0fa88edb682c935860c98816f
dokimion24 <leecy3610@gmail.com>
2023-06-02 21:16:45 +0900

    design: global 스타일 지정

---
+++ src/styles/global.jsx
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+* {
+    box-sizing: border-box;
+  }
+
+  body {
+    display: flex;
+    align-items:center;
+    justify-content: center;
+    background-color: #C7D1DB;
+  }
+`;
