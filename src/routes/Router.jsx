09f1bc6609eefacab6b053ae0206c1cee72f74f3
dokimion24 <leecy3610@gmail.com>
2023-06-02 21:17:32 +0900

    feat: Router 공통 Layout 컴포넌트 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,13 +3,16 @@ import { Route, Routes } from "react-router-dom";
 import SigninPage from "../page/SigninPage";
 import SignupPage from "../page/SignupPage";
 import TodoPage from "../page/Todo";
+import Layout from "../components/Common";
 
 function Router() {
   return (
     <Routes>
-      <Route path="/signin" element={<SigninPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/todo" element={<TodoPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="/signin" element={<SigninPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/todo" element={<TodoPage />} />
+      </Route>
     </Routes>
   );
 }
