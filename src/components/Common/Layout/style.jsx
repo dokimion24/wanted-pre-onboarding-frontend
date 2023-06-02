243f8cf3c1f7fcbd6f28235f8124823a1aedfa0c
dokimion24 <leecy3610@gmail.com>
2023-06-03 00:12:48 +0900

    feat: Layout 컴포넌트 폴더 경로 변경 Common/ -> Common/Layout/

--- src/components/Common/Layout/style.jsx
+++ src/components/Common/Layout/style.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   width: 425px;
   height: 100vh;
-  border-radius: 4px;
+  position: relative;
+  border-radius: 8px;
   background-color: #fff;
 `;
