0fcd56e429adad142b5253d0f39ceb4b79a0c0cf
dokimion24 <leecy3610@gmail.com>
2023-06-02 21:37:05 +0900

    design: 스타일 reset, button style reset

--- src/styles/global.jsx
+++ src/styles/global.jsx
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 export const GlobalStyle = createGlobalStyle`
+ ${reset}
 * {
     box-sizing: border-box;
   }
@@ -10,5 +12,12 @@ export const GlobalStyle = createGlobalStyle`
     align-items:center;
     justify-content: center;
     background-color: #C7D1DB;
+  }
+
+  button {
+    background: transparent;
+    border: none;
+    padding: 0;
+    cursor: pointer;
   }
 `;
