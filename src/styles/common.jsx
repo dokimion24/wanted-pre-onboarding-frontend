20a83236cc38f3c5b0bda208623d8890f9347ee1
dokimion24 <leecy3610@gmail.com>
2023-06-03 00:16:47 +0900

    design: 공통 버튼 스타일 지정

---
+++ src/styles/common.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  font-weight: bold;
+  color: #fff;
+  background-color: #3399ff;
+  padding: 8px;
+  border-radius: 8px;
+
+  &:hover {
+    background-color: #237fdf;
+  }
+`;
