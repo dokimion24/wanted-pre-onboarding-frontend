72b67784915bb55ba8fe0a1e169a815eac915774
dokimion24 <leecy3610@gmail.com>
2023-06-04 19:55:26 +0900

    feat: signup api 사용하여 회원가입 기능 구현
    - 회원가입 성공 시 signin 페이지 이동
    - 회원가입 실패 시 에러 메세지 alert

--- src/components/SignupForm/index.jsx
+++ src/components/SignupForm/index.jsx
@@ -1,32 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style";
 import { AiOutlineLogin } from "react-icons/ai";
 import { Button } from "../../styles/common";
+import { useValidateInput } from "../../hooks/useValidteInput";
+import { signupUser } from "../../apis/auth";
+import { useNavigate } from "react-router-dom";
 
-function SignupForm() {
+function SigninForm() {
+  const validateInput = useValidateInput();
+  const [userInput, setUserInput] = useState({ email: "", password: "" });
+  const [isValidInput, setIsValidInput] = useState({
+    email: false,
+    password: false,
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    // console.log(e.target);
+    const { id, value } = e.target;
+    setUserInput({
+      ...userInput,
+      [id]: value,
+    });
+
+    setIsValidInput(validateInput(id, value, isValidInput));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await signupUser(userInput);
+    console.log(res);
+    if (res.status === 201) {
+      alert("회원가입 성공");
+      navigate("/signin");
+    } else {
+      alert(`${res.response.data.message}`);
+    }
+  };
+
   return (
-    <S.Form>
+    <S.Form onSubmit={handleSubmit}>
       <S.IconWrapper>
         <AiOutlineLogin />
         <S.SigninText>Sign up</S.SigninText>
       </S.IconWrapper>
       <S.InputArea>
         <S.Label htmlFor="email">이메일</S.Label>
-        <S.Input id="email" data-testid="email-input" />
+        <S.Input
+          id="email"
+          onChange={handleChange}
+          value={userInput.email}
+          data-testid="email-input"
+        />
+        {!isValidInput.email && (
+          <S.ErrorMessage>이메일에 @을 포함하세요</S.ErrorMessage>
+        )}
       </S.InputArea>
       <S.InputArea>
         <S.Label htmlFor="password">비밀번호</S.Label>
-        <S.Input id="password" data-testid="password-input" />
-      </S.InputArea>
-      <S.InputArea>
-        <S.Label htmlFor="passwordConfirm">비밀번호 확인</S.Label>
-        <S.Input id="passwordConfirm" data-testid="password-input" />
+        <S.Input
+          id="password"
+          type="password"
+          onChange={handleChange}
+          value={userInput.password}
+          data-testid="password-input"
+        />
+        {!isValidInput.password && (
+          <S.ErrorMessage>비밀번호를 8자리 이상 입력하세요</S.ErrorMessage>
+        )}
       </S.InputArea>
       <S.ButtonWrapper>
-        <Button data-testid="signin-button">회원 가입</Button>
+        <Button
+          type="submit"
+          disabled={!isValidInput.email || !isValidInput.password}
+          data-testid="signin-button">
+          로그인
+        </Button>
       </S.ButtonWrapper>
     </S.Form>
   );
 }
 
-export default SignupForm;
+export default SigninForm;
