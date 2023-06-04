34933c4de18c007e4f6af916ad4fad50617dab8a
dokimion24 <leecy3610@gmail.com>
2023-06-04 20:06:57 +0900

    feat: accessToken이 있으면 header 포함하여 요청 보내는 interceptors 작성

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -6,3 +6,16 @@ export const axiosInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+axiosInstance.interceptors.request.use(
+  (request) => {
+    const accessToken = localStorage.getItem("accessToken");
+
+    if (accessToken) request.headers["Authorization"] = `Bearer ${accessToken}`;
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
