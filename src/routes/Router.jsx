fc88856f73c77209e867b40b00e88626d2563ab7
dokimion24 <leecy3610@gmail.com>
2023-06-04 21:19:18 +0900

    feat: PrivateRoute 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,14 +4,17 @@ import SigninPage from "../page/SigninPage";
 import SignupPage from "../page/SignupPage";
 import TodoPage from "../page/Todo";
 import Layout from "../components/Common/Layout";
+import PrivateRoute from "./PrivateRoute";
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="/signin" element={<SigninPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/todo" element={<TodoPage />} />
+        <Route path="/" element={<PrivateRoute />}>
+          <Route path="/signin" element={<SigninPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/todo" element={<TodoPage />} />
+        </Route>
       </Route>
     </Routes>
   );
