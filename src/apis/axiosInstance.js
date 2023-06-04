a72dc1691a3c9e6986d61dd76d114a3dc2f20c53
dokimion24 <leecy3610@gmail.com>
2023-06-04 19:35:31 +0900

    fix: env 적용 되지않는 문제 해결

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,10 +1,7 @@
 import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export const axiosInstance = axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
