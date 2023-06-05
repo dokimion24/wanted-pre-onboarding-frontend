4d27b0e0abf73522a3f89fcce2a0e09253f4c6dc
dokimion24 <leecy3610@gmail.com>
2023-06-05 14:23:16 +0900

    feat: 작성한 todo 만큼 todo 생성

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -2,20 +2,23 @@ import React from "react";
 import * as S from "./style";
 import { Button } from "../../styles/common";
 
-function TodoList() {
+function TodoList({ todos }) {
+  console.log(todos.todo);
   return (
-    <>
-      <S.TodoWrapper>
-        <label>
-          <input type="checkbox" />
-          <span>TODO 1</span>
-        </label>
-        <S.ButtonWrapper>
-          <Button data-testid="modify-button">수정</Button>
-          <Button data-testid="delete-button">삭제</Button>
-        </S.ButtonWrapper>
-      </S.TodoWrapper>
-    </>
+    <ul>
+      {todos.map((todo) => (
+        <S.Todo key={todo.id}>
+          <label>
+            <input type="checkbox" />
+            <span>{todo.todo}</span>
+          </label>
+          <S.ButtonWrapper>
+            <Button data-testid="modify-button">수정</Button>
+            <Button data-testid="delete-button">삭제</Button>
+          </S.ButtonWrapper>
+        </S.Todo>
+      ))}
+    </ul>
   );
 }
 
