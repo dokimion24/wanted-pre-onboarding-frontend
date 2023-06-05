bf9022990149ebb82d76f0c3a4519f4656be1263
dokimion24 <leecy3610@gmail.com>
2023-06-05 14:21:57 +0900

    feat: api 요청을 통해 todolist를 가져오기

--- src/page/Todo/index.jsx
+++ src/page/Todo/index.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TodoHeader from "../../components/TodoHeader";
 import * as S from "./style";
 import TodoList from "../../components/TodoList";
+import { getTodos } from "../../apis/todo";
 
 function TodoPage() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const res = await getTodos();
+      setTodos(res);
+    };
+
+    fetchTodos();
+  }, []);
+
   return (
     <S.Wrapper>
       <TodoHeader />
-      <TodoList />
+      <TodoList todos={todos} />
     </S.Wrapper>
   );
 }
