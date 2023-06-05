0aea4f63135caf696ad2bda53a4a32c76fdf2ebc
dokimion24 <leecy3610@gmail.com>
2023-06-05 11:49:21 +0900

    design: TodoHeader 컴포넌트 스타일 지정

---
+++ src/components/TodoHeader/style.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Header = styled.h1`
+  font-size: 32px;
+  font-weight: 600;
+`;
+
+export const InputArea = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Input = styled.input`
+  border: none;
+  border-bottom: 1px solid #ccc;
+  margin-right: 8px;
+  height: 30px;
+`;
