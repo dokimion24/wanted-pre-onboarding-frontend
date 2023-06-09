1abe12cdc18e04d5e4cfac61104d5b72ba4389b5
dokimion24 <leecy3610@gmail.com>
2023-06-09 21:01:44 +0900

    design: 로딩 애니메이션 스타일 지정

---
+++ src/components/Common/Loading/style.jsx
@@ -0,0 +1,25 @@
+import styled, { keyframes } from "styled-components";
+import { FaSpinner } from "react-icons/fa";
+
+const spinAnimation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const SpinnerIcon = styled(FaSpinner)`
+  animation: ${spinAnimation} 1s linear infinite;
+`;
+
+export const LoadingWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  font-size: 36px;
+  color: #333;
+  background-color: #f5f5f5;
+`;
