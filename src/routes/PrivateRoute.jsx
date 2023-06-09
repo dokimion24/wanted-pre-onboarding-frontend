ecbe1ffc83d45205ad24e686f34d9c30f26e580f
dokimion24 <leecy3610@gmail.com>
2023-06-09 19:40:19 +0900

    refactor: 객체 구조 분해 할당

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import { Outlet, Navigate, useLocation } from "react-router-dom";
 
 function PrivateRoute() {
-  const location = useLocation();
-  console.log(location.pathname);
+  const { pathname } = useLocation();
   const accessToken = localStorage.getItem("accessToken");
 
-  if (location.pathname === "/todo") {
+  if (pathname === "/todo") {
     return accessToken ? <Outlet /> : <Navigate to="/signin" replace />;
   } else {
     return accessToken ? <Navigate to="/todo" replace /> : <Outlet />;
