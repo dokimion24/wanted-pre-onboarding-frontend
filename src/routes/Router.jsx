03387c22f392a67379722d5f9db6cda814fe1c3a
dokimion24 <leecy3610@gmail.com>
2023-06-09 19:40:00 +0900

    feat: NotFoundPage 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -5,15 +5,17 @@ import SignupPage from "../page/SignupPage";
 import TodoPage from "../page/Todo";
 import Layout from "../components/Common/Layout";
 import PrivateRoute from "./PrivateRoute";
+import NotFoundPage from "../page/NotFoundPage";
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="/" element={<PrivateRoute />}>
+        <Route element={<PrivateRoute />}>
           <Route path="/signin" element={<SigninPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/todo" element={<TodoPage />} />
+          <Route path="/*" element={<NotFoundPage />} />
         </Route>
       </Route>
     </Routes>
