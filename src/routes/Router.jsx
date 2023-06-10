30c34af078f9744fdf89465aa8efcc986153f1c9
dokimion24 <leecy3610@gmail.com>
2023-06-10 19:31:56 +0900

    feat: 홈 페이지 HomePage 컴포넌트 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,17 +1,19 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import SigninPage from "../page/SigninPage";
-import SignupPage from "../page/SignupPage";
-import TodoPage from "../page/Todo";
-import Layout from "../components/Common/Layout";
-import PrivateRoute from "./PrivateRoute";
-import NotFoundPage from "../page/NotFoundPage";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import SigninPage from '../page/SigninPage';
+import SignupPage from '../page/SignupPage';
+import TodoPage from '../page/TodoPage';
+import Layout from '../components/Common/Layout';
+import PrivateRoute from './PrivateRoute';
+import NotFoundPage from '../page/NotFoundPage';
+import HomePage from '../page/HomePage';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route element={<PrivateRoute />}>
+          <Route path="/" element={<HomePage />} />
           <Route path="/signin" element={<SigninPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/todo" element={<TodoPage />} />
