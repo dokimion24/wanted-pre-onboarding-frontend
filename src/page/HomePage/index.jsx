9c6c23094514fc23ecbf6d23a759abbe8bb963f2
dokimion24 <leecy3610@gmail.com>
2023-06-10 20:42:18 +0900

    feat: 홈페이지 레이아웃, 로그인 회원가입 페이지 이동 기능 구현

--- src/page/HomePage/index.jsx
+++ src/page/HomePage/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Wrapper } from '../../styles/common';
+import { Button, Wrapper } from '../../styles/common';
 import * as S from './style';
+import { useNavigate } from 'react-router-dom';
 
 function HomePage() {
+  const navigate = useNavigate();
   return (
     <Wrapper>
-      <S.Title>HomePage</S.Title>
+      <S.Title>Lee Chan Young</S.Title>
+      <S.ButtonWrapper>
+        <Button onClick={() => navigate('signin')}>로그인</Button>
+        <Button onClick={() => navigate('signup')}>회원가입</Button>
+      </S.ButtonWrapper>
     </Wrapper>
   );
 }
