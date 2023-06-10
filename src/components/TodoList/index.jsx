910945c0e0a476f7e8aefaf6882f0a8d83c6ed5d
dokimion24 <leecy3610@gmail.com>
2023-06-10 20:25:41 +0900

    feat: TodoContainer 스타일 컴포넌트 적용

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import * as S from "./style";
+import React from 'react';
+import * as S from './style';
 
-import TodoItem from "../TodoItem";
+import TodoItem from '../TodoItem';
 
 function TodoList({ todos, setTodos }) {
   return (
-    <ul>
+    <S.TodoContainer>
       {todos.map((todo) => (
         <TodoItem key={todo.id} todo={todo} todos={todos} setTodos={setTodos} />
       ))}
-    </ul>
+    </S.TodoContainer>
   );
 }
 
